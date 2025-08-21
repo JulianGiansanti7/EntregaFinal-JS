14c35db4fa74c61128590a002170a4cf120b3274
juliangiansanti <juliangiansanti@gmail.com>
2025-08-21 00:33:20 -0300

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const guitarrasContainer = document.getElementById('guitarras_container');
+
+fetch("./data/data.json")
+.then(response => response.json())
+.then( data => {
+    const guitarras = data.guitarras
+
+    guitarras.forEach( guitarra => {
+        const guitarraElement = document.createElement("div")
+        guitarraElement.innerHTML = `
+        <div class="guitar__cards">
+            <div class="guitar__cards--info">
+                <a href="#" class="guitar-link">
+                    <img src="${guitarra.img}" alt="${guitarra.tipo}" />
+                    <h3 class="guitar-title"> ${guitarra.nombre} </h3>
+                    <p class="guitar-price">Lista: U$${guitarra.precio}</p>
+                </a>
+            </div>
+            <button id="btn" class="guitar-button">Agregar a Carrito</button>
+        </div>
+        `
+
+        guitarrasContainer.appendChild(guitarraElement)
+        guitarrasContainer.className = ("guitarras_container")
+    });
+}).catch( error => {
+    console.error("Ha ocurrido un error... " + error)
+});
